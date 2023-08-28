862a92c7da15fa809c0925428267b40a0bcaf340
N V S Pavan Kalyan <pavankalyannvs.chaluwadi@gmail.com>
2023-08-28 21:00:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
+console.log("Hello, World!");
+        console.log(5 + 10);
+        let greeting = "Hello, JavaScript!";
+        console.log(greeting);
+        console.log("Hi there, Myself Pavan Kalyan ");//Print My name
+        console.log(5 + 3); // Outputs: 8 (Sum of 5 and 3)
+        console.log(10 - 4); // Outputs: 6 (Difference of 10 and 4)
+        console.log(6 * 2); // Outputs: 12 (Product of 6 and 2)
+        console.log(20 / 5); // Outputs: 4 (Division of 20 by 5)
+        let city = "My Favorite City is Banglore";
+        console.log(city);// Name of your favorite city
 
+        let a = 7;
+        let b = 3;
+        let sum = a + b;
+        console.log("Sum:", sum);
+        
+        // Write a function called sayHello
+        function sayHello(name) {
+            console.log("Hello, " + name + "!");
+        }
+
+        // Call the function with different names
+        sayHello("Pavan");
+        sayHello("Sai");
+
+        // Write a function for subtraction
+        function sub(a, b) {
+            return a - b;
+        }
+
+        // Call the function with different numbers
+        let res1 = sub(10, 3);
+        let res2 = sub(8, 5);
+        console.log(res1); // Outputs: 7
+        console.log(res2); // Outputs: 3
+
+        // Declare a constant for your birth year
+        const birthYear = 1999;
+
+        // Declare an array of your favorite foods
+        let favoriteFoods = ["Chili Garlic  Paneer", "Crispy Vegetable Pakoras", "Dosa"];
+
+        // Create an object that describes a book
+        let book = {
+            title: "The Almanack Of Naval Ravikant: A Guide to Wealth and Happiness",
+            author: " Eric Jorgenson",
+            publicationYear: 2020
+        };
